1878ac07d387a36a012724fd88b0c2d7e4a56ad0
PRIYESH41 <94980940+PRIYESH41@users.noreply.github.com>
2026-02-21 17:20:28 +0530

    Add Student class with properties and methods

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student{
+  private int id;
+  private String name;
+  private String address;
+
+  public int getId(){ return this.id;}
+  public String getName(){ return this.name;}
+  public String getAddress(){ return this.address;}
+
+  public void setId(int id){ this.id = id;}
+  public void setId(String name){ this.name = name;}
+  public void setId(String address){ this.address = address;}
+}
